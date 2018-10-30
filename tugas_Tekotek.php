7cf5a4613fc22c1da295d244035edc336aab5b05
kuresih <42989772+kuresih@users.noreply.github.com>
2018-10-30 19:12:20 +0700

    pembaharuan

--- tugas_Tekotek.php
+++ tugas_Tekotek.php
@@ -7,12 +7,11 @@
 	<?php
 	echo "<font face='Helvatica'>Tekotek anak ayam turun 100</font> <br>";
 	$tekotekotek = 99;
-	for ($i=$tekotekotek;$i > 0; $i--) {
-		if($i % 2 == 0){
-			echo "<font face ='Helvatica'><font color='#964B00'>Mati satu tinggal $i</font>"; echo "<br>";
-		} 
-	else{
-		echo "<font face ='Helvatica'><font color='FF8C00'>Mati satu tinggal $i</font>"; echo "<br>";
+	for ($a=$tekotekotek;$a > 0; $a--) {
+		if($a % 2 == 0){
+			echo '<p style="color:orange;font-family:"Helvatica";">'."Mati satu tinggal $a"; echo "</p>";
+		}else{
+		echo '<p style="color:brown;font-family:"Helvatica";">'."Mati satu tinggal $a"; echo "</p>";
 			}
 
 		}
