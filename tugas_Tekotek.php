b847b505333ab0c300196a23b01a584d3a608be2
kuresih <42989772+kuresih@users.noreply.github.com>
2018-10-30 19:55:56 +0700

    perubahan

--- tugas_Tekotek.php
+++ tugas_Tekotek.php
@@ -5,13 +5,13 @@
 </head>
 <body>
 	<?php
-	echo "<font face='Helvatica'>Tekotek anak ayam turun 100</font> <br>";
+	echo "<font face='Helvetica'>Tekotek anak ayam turun 100</font> <br>";
 	$tekotekotek = 99;
 	for ($a=$tekotekotek;$a > 0; $a--) {
 		if($a % 2 == 0){
-			echo '<p style="color:orange;font-family:"Helvatica";">'."Mati satu tinggal $a"; echo "</p>";
+			echo '<p style="color:orange;font-family: Helvetica;">'."Mati satu tinggal $a"; echo '</p>';
 		}else{
-		echo '<p style="color:brown;font-family:"Helvatica";">'."Mati satu tinggal $a"; echo "</p>";
+		echo '<p style="color:brown;font-family:Helvetica;">'."Mati satu tinggal $a"; echo "</p>";
 			}
 
 		}
